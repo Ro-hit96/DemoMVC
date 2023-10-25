9229624f49a55caaedc9be30713fd3bcd0a57ca7
StartlordR <rohitsurvase4363@gmail.com>
2023-10-25 21:22:36 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["message"] = "This is sample text in Viewdata";
+            List<string> names = new List<string>()
+            {
+                "Akashay","Amol","Tushar","Vivek",
+            };
+            ViewData["list"] = names;//implicit -->Object
+                                     ////////////////////////////////////////////////////////////////////////////////////
+            /*ViewData["message"] = "This is sample Cities in Viewdata";
+            List<string> city = new List<string>()
+            {
+                "pune","Mumbai","ENGland","AustS",
+            };
+            ViewData["list"] = city;*/
+
+            //Working With ViewBag
+            ViewBag.Year = 2023;
+            ViewBag.Month = "Oct";
+            ViewBag.Names= names;
+
+            TempData["company"] = "Think Quotient";
+            TempData.Keep("Compant");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+
+            return View();
+        }
+
+        public IActionResult AboutUS()
+        {
+            return View();
+        }
+        public IActionResult ContactUS()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/PersonalController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DemoMVC.Controllers
+{
+    public class PersonalController : Controller
+    {
+        public IActionResult PersonalDetails()
+        {
+            List<string> cities = new List<string>()
+            {
+                "Pune","Mumbai","Nashik","Sangli","Satara","Solapur"
+            };
+
+
+            ViewData["cities"] = new SelectList(cities);
+
+            return View();
+        }
+    }
+}
